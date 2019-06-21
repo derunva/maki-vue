f020a8dc6de94f1723a4e04ef14da59f8a1a260b
derun <derun.va@gmail.com>
2019-06-21 18:39:23 +0300

    vuex start

--- src/main.js
+++ src/main.js
@@ -16,5 +16,26 @@ Vue.filter(
 )
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
+import $ from "jquery";
+import ionRangeSlider from 'ion-rangeslider'
+import store from './store'
+jQuery(document).ready(function(){
+  
+   $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 1000,
+    max: 10000,
+    step: 10,
+    hide_from_to: true,
+    hide_min_max: true,
+
+    onChange: function (data) {
+      console.dir(data.from);
+      $('.price-from').text(data.from + ' руб.');
+      $('.price-to').text(data.to + ' руб.');
+    }
+  });
+})
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,23 @@
 <template lang="pug">
   <div class="home">
+    .wrapper
+      .row
+        .column-laptop-3-4
+          ProductsListing
     ArticleListing
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import ArticleListing from '../components/ArticleListing.vue'
-
+import ProductsListing from '../components/ProductsListing.vue'
 export default {
   name: 'home',
   components: {
-    ArticleListing
+    ArticleListing,
+    ProductsListing
   }
 }
 </script>
